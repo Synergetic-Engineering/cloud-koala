bf4b38c1af07f82b664fa8822680d29a887858eb
Michael Barbera <michael.barbera96@gmail.com>
2017-11-27 12:27:47 +1000

    using dummy names for aws resources
    tests will fail until we work out how to mock the resources

--- test/util.py
+++ test/util.py
@@ -2,5 +2,5 @@
 # Figure there'll be a better way to do this once we use moto?
 import os
 # XXX - shouldn't actually be deployed resources, the should be mock ones using moto
-os.environ['DYNAMODB_TABLE'] = 'cloud-koala-dev-models'
-os.environ['S3_BUCKET'] = 'cloud-koala-dev-models'
+os.environ['DYNAMODB_TABLE'] = 'moto-test-table'
+os.environ['S3_BUCKET'] = 'moto-test-bucket'
