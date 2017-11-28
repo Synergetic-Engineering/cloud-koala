20fdb10ce355feaacac878e2e1ec326302397887
Michael Barbera <michael.barbera96@gmail.com>
2017-11-28 16:58:11 +1000

    archiving deleted s3 resources

--- lib.py
+++ lib.py
@@ -16,7 +16,7 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(os.environ['S3_BUCKET'])
-
+s3_client = boto3.client('s3')
 
 def list_models():
     """
@@ -57,11 +57,35 @@ def delete_model(model_id):
     """
     Delete the model record from S3 and either archive or delete related files in S3
     """
+
     result = table.delete_item(
         Key={
             'model_id': model_id,
         }
     )
+    try:
+        # Archive Objects
+        bucket.put_object(
+            Body=bucket.Object('excel_uploads/{}'.format(model_id)).get()['Body'].read(),
+            Key='excel_uploads_archive/{}'.format(model_id),
+        )
+    except:
+       pass
+    try:    
+        bucket.put_object(
+            Body=bucket.Object('compiled_models/{}'.format(model_id)).get()['Body'].read(),
+            Key='compiled_models_archive/{}'.format(model_id),
+        )
+    except:
+        pass
+    # Delete Objects
+    bucket.delete_objects(Delete={
+        'Objects': [
+            {'Key': 'excel_uploads/{}'.format(model_id)},
+            {'Key': 'compiled_models/{}'.format(model_id)},
+        ]
+    })
+
     # TODO Check for error
     return model_id
 
