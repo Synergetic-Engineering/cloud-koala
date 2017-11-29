fa6d22c5132305b2736e6d129cae08807ff0807f
Michael Barbera <michael.barbera96@gmail.com>
2017-11-29 13:22:40 +1000

    split add_model and update_model tests

--- test/test_lib.py
+++ test/test_lib.py
@@ -34,6 +34,10 @@ def _get_table_model_parameter(model_id, parameter):
         if each['model_id']==model_id:
             return each[parameter]
 
+def _get_file_string():
+    with open('test/test.xlsx', 'r') as f:
+        return f.read()
+
 
 @mock_dynamodb2
 @mock_s3
@@ -45,16 +49,22 @@ def test_list_models():
 
 @mock_dynamodb2
 @mock_s3
-def test_add_or_update_model():
+def test_add_model():
     test.util.create_dynamodb_table()
     test.util.create_s3_bucket()
-    with open('test/test.xlsx', 'r') as f:
-        file_string = f.read()
+    file_string = _get_file_string()
     model_id = lib.add_or_update_model('file_name', file_string)
     # check model_id is in dynamodb and s3
     assert model_id in _get_table_model_ids()
     assert 'excel_uploads/{}'.format(model_id) in _get_bucket_model_ids()
-    
+
+
+@mock_dynamodb2
+@mock_s3
+def test_update_model():
+    test.util.create_dynamodb_table()
+    test.util.create_s3_bucket()
+    file_string = _get_file_string()
     lib.add_or_update_model('file_name', file_string, model_id='123abc')
     # check model_id is still in dynamodb and s3
     assert '123abc' in _get_table_model_ids()
@@ -62,12 +72,6 @@ def test_add_or_update_model():
     # check version==2
     assert _get_table_model_parameter('123abc', 'version')==str(2)
 
-    lib.add_or_update_model('file_name', file_string, model_id)
-    # check model_id is still in dynamodb and s3
-    assert model_id in _get_table_model_ids()
-    assert 'excel_uploads/{}'.format(model_id) in _get_bucket_model_ids()
-    # check version==2
-    assert _get_table_model_parameter(model_id, 'version')==str(2)
 
 @mock_dynamodb2
 @mock_s3
@@ -82,7 +86,7 @@ def test_get_model():
 def test_delete_model():
     test.util.create_dynamodb_table()
     test.util.create_s3_bucket()
-    lib.delete_model('123abc')
+    lib.delete_model('123abc') 
 
 
 @mock_dynamodb2
