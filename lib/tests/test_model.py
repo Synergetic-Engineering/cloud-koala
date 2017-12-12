f819e931024209bdacc406b0be37f2457ace3809
srp-synengco <sam.patterson@synengco.com>
2017-12-12 12:44:48 +1000

    improved and cleaned up tests

--- lib/tests/test_model.py
+++ lib/tests/test_model.py
@@ -16,18 +16,8 @@ s3 = boto3.resource('s3')
 bucket = s3.Bucket(os.environ['S3_BUCKET'])
 
 
-def _get_table_model_ids():
-    return [each['model_id'] for each in table.scan()['Items']]
-
-
 def _get_bucket_model_ids():
     return [each.key for each in bucket.objects.all()]
-
-
-def _get_table_model_parameter(model_id, parameter):
-    for each in table.scan()['Items']:
-        if each['model_id'] == model_id:
-            return each[parameter]
 
 
 def _get_file_string():
@@ -44,8 +34,13 @@ def test_list_models():
 def test_add_model():
     file_string = _get_file_string()
     model_id = lib.model.add_or_update_model('file_name', file_string)
-    # check model_id is in dynamodb and s3
-    assert model_id in _get_table_model_ids()
+    # check dynamodb record
+    dynamodb_record = table.get_item(Key={'model_id': model_id})['Item']
+    assert dynamodb_record['file_name'] == 'file_name'
+    assert dynamodb_record['compilation_status'] == 'Waiting'
+    assert dynamodb_record['config_info'] == {'err': 'Config sheet `cloud-koala-config` not found'}
+    assert dynamodb_record['version'] == '1'
+    # check model is in s3
     assert 'excel_uploads/{}'.format(model_id) in _get_bucket_model_ids()
 
 
@@ -53,11 +48,14 @@ def test_add_model():
 def test_update_model():
     file_string = _get_file_string()
     lib.model.add_or_update_model('file_name', file_string, model_id='123abc')
-    # check model_id is still in dynamodb and s3
-    assert '123abc' in _get_table_model_ids()
+    # check dynamodb record
+    dynamodb_record = table.get_item(Key={'model_id': '123abc'})['Item']
+    assert dynamodb_record['file_name'] == 'file_name'
+    assert dynamodb_record['compilation_status'] == 'Waiting'
+    assert dynamodb_record['config_info'] == {'err': 'Config sheet `cloud-koala-config` not found'}
+    assert dynamodb_record['version'] == '2'
+    # check model is in s3
     assert 'excel_uploads/123abc' in _get_bucket_model_ids()
-    # check version==2
-    assert _get_table_model_parameter('123abc', 'version') == str(2)
 
 
 @util.setup_mock_resources
@@ -69,7 +67,7 @@ def test_get_model():
 def test_delete_model():
     print lib.model.delete_model('123abc')
     # check out of dynamodb table
-    assert '123abc' not in _get_table_model_ids()
+    assert 'Item' not in table.get_item(Key={'model_id': '123abc'})
     # S3 Bucket Checks
     # check out of excel_uploads
     assert 'excel_uploads/123abc' not in _get_bucket_model_ids()
